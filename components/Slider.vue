81cee1ca3755378fa7969adbd4073f3579628a47
essozmdr1 <88781587+essozmdr1@users.noreply.github.com>
2022-12-04 01:31:06 +0300

    second commit
    component eklendi

---
+++ components/Slider.vue
@@ -0,0 +1,7 @@
+<template> 
+<div class="slider-home" data-analyticskey="Hero Banner - https://www.superstep.com.tr/nike/" style="float: left; list-style: none; position: relative; width: 1903px;" aria-hidden="false">
+          <a href="https://www.superstep.com.tr/nike/" class="home-slider__img-box js-slider-slug-">
+            <img loading="lazy" src="https://cdn-ss.akinon.net/cms/2022/11/16/3ed1d39f-1ce5-4d2b-a23f-e73b9ca383ce.jpg" alt="Nike" class="home-slider__img">
+          </a>
+        </div>
+</template>
